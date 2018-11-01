1f8c6ca80eefb4d434187baa3e5b77db8e8c3638
KangZhengD <beardou@126.com>
2018-11-01 10:30:42 +0800

    rrre

--- OpsManage/accounts/urls.py
+++ OpsManage/accounts/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^editAdmin$', views.editAdmin),
     url(r'^setPassword$', views.setPassword),
-    url(r'^newPasswordSubmit$',views.newPasswordSubmit)
+    url(r'^newPasswordSubmit$',views.newPasswordSubmit),
+    # url(r'^login',views.login_view),
 ]
